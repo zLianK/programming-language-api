4edd3bba787e137283a30635b4257639659c795d
Lían König <lk.liankonig@gmail.com>
2022-07-21 17:24:54 -0300

    starting spring project

---
+++ src/main/java/com/lk/programming/languages/api/ProgrammingLanguagesApiApplication.java
@@ -0,0 +1,13 @@
+package com.lk.programming.languages.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingLanguagesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammingLanguagesApiApplication.class, args);
+	}
+
+}
